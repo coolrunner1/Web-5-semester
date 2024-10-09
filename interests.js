1e71cde42d8ec0fe4811ce945a2df47f406b43c7
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-10-09 12:30:57 +0300

    finished interests js script

---
+++ interests.js
@@ -0,0 +1,19 @@
+const displayInterests = (...interests) => {
+    const container = document.getElementById('interests-container');
+    const list = document.createElement(interests[0]+'l');
+    const lh = document.createElement("lh");
+    lh.textContent="Содержимое";
+    list.appendChild(lh);
+    for(let i=1; i<interests.length; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = interests[i];
+        a.href="#int"+i;
+        a.classList="hero-secondary-but black-but";
+        li.appendChild(a);
+        list.appendChild(li);
+    }
+    container.appendChild(list);
+}
+
+displayInterests("o","Одним из моих главных хобби являются игры", "Я активно изучаю английский язык", "Книги занимают особое место в моем сердце", "Фильмы тоже играют важную роль в моей жизни");
