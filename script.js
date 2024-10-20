1d0ac2dab25404c5d806e4792301115279abe4ef
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-10-20 20:06:32 +0300

    Added clocks to the bottom of the page

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 if (screen.width < 1282){
     alert("Your screen is not supported! Website might not work as intended!")
 }
+const clockElement = document.createElement("div");
+clockElement.id = "clock";
+document.querySelector("footer").prepend(clockElement);
+
+
+const setTime = () => {
+    const date = new Date();
+    clockElement.textContent = date.toLocaleString("ru-RU");
+    setTimeout(setTime, 1000);
+}
+
+setTime();
