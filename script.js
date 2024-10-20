3e0a9fab5dfed931db10fd7e980202f015b6ef5f
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-10-20 23:06:22 +0300

    Storage logic is now asyncronous

--- script.js
+++ script.js
@@ -14,32 +14,39 @@ if (screen.width < 1282){
     console.log(localStorage.getItem(pageName));
 };*/
 
-const cookieToObject = (cookieString) => {
-    console.log(cookieString);
-    if(!cookieString){
-        return {};
-    }
-    let cookies = cookieString.split("; ");
-    let newCookies = [];
-    cookies.forEach(cookie => {
-        cookie = cookie.split("=");
-        cookie[0]='"'+cookie[0]+'"';
-        cookie[1]='"'+cookie[1]+'"';
-        cookie=cookie[0]+":"+cookie[1];
-        newCookies.push(cookie);
-    })
-    let cookiesJSON="";
-    newCookies.map(cookie => {
-        cookiesJSON += cookie;
-        cookiesJSON += ", ";
-    })
-    cookiesJSON=cookiesJSON.substring(0, cookiesJSON.length - 2);
-    cookiesJSON = "{"+cookiesJSON+"}";
-    console.log(cookiesJSON);
-    return JSON.parse(cookiesJSON);
-}
+const cookieToObject = async (cookieString) => {
+    return new Promise((resolve, reject) => {
+        try {
+            console.log(cookieString);
+            if(!cookieString){
+                return {};
+            }
+            let cookies = cookieString.split("; ");
+            let newCookies = [];
+            cookies.forEach(cookie => {
+                cookie = cookie.split("=");
+                cookie[0]='"'+cookie[0]+'"';
+                cookie[1]='"'+cookie[1]+'"';
+                cookie=cookie[0]+":"+cookie[1];
+                newCookies.push(cookie);
+            })
+            let cookiesJSON="";
+            newCookies.map(cookie => {
+                cookiesJSON += cookie;
+                cookiesJSON += ", ";
+            })
+            cookiesJSON=cookiesJSON.substring(0, cookiesJSON.length - 2);
+            cookiesJSON = "{"+cookiesJSON+"}";
+            console.log(cookiesJSON);
+            resolve("Cookies have been converted to JSON successfully!");
+            return JSON.parse(cookiesJSON);
+        } catch (error) {
+            reject(`Failed to set cookie: ${error}`);
+        }
+    });
+};
 
-const registerVisit = (pageName) => {
+const registerVisit = async(pageName) => {
     let visits = sessionStorage.getItem(pageName);
     if (visits) {
         sessionStorage.setItem(pageName, (parseInt(visits)+1).toString())
@@ -47,53 +54,71 @@ const registerVisit = (pageName) => {
     else{
         sessionStorage.setItem(pageName, "1");
     }
-    if (!getCookie(pageName)) {
-        setCookie(pageName, "1", 10);
+    if (!await getCookie(pageName)) {
+        await setCookie(pageName, "1", 10);
     }
     else{
-        updateCookieCount(pageName);
+        await updateCookieCount(pageName);
     }
     console.log(sessionStorage);
     console.log(sessionStorage.getItem(pageName));
     console.log(getCookie(pageName));
 };
 
-const setCookie = (name, value, expirationDays) => {
-    let date=new Date();
-    date.setDate(date.getDate()+expirationDays);
-    document.cookie = name + "=" + value + ";path=/; expires=" + date.toUTCString();
+const setCookie = async (name, value, expirationDays) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let date = new Date();
+            date.setDate(date.getDate() + expirationDays);
+            document.cookie = `${name}=${value}; path=/; expires=${date.toUTCString()}`;
+            resolve(`Cookie "${name}" set successfully.`);
+        } catch (error) {
+            reject(`Failed to set cookie: ${error}`);
+        }
+    });
 };
 
-const getCookie = (name) => {
-    let cookieName = name + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+const getCookie = async (name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cookieName = name + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for(let i = 0; i <ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(cookieName) == 0) {
+                    resolve("Cookie has been found successfully.");
+                    return c.substring(cookieName.length, c.length);
+                }
+            }
+            resolve("Cookie has not been found.");
+            return "";
+        } catch (error) {
+            reject(`Failed to get cookie: ${error}`);
         }
-        if (c.indexOf(cookieName) == 0) {
-            return c.substring(cookieName.length, c.length);
-        }
-    }
-    return "";
+    });
 };
 
-const updateCookieCount = (name) => {
-    let cookieObject = cookieToObject(document.cookie.toString());
+
+const updateCookieCount = async (name) => {
+    let cookieObject = await cookieToObject(document.cookie.toString());
     cookieObject[name]=(parseInt(cookieObject[name])+1).toString();
-    setCookie(name, cookieObject[name], 10);
+    await setCookie(name, cookieObject[name], 10);
 };
 
 const clockElement = document.createElement("div");
 clockElement.id = "clock";
 document.querySelector("footer").prepend(clockElement);
 
-const setTime = () => {
+const setTime = async() => {
     const date = new Date();
     clockElement.textContent = date.toLocaleString("ru-RU");
-    setTimeout(setTime, 1000);
+    setTimeout(await setTime, 1000);
 }
 
-setTime();
+(async () => {
+    await setTime();
+})();
