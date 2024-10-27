84249ed06b2e3f7ce9f4be5677a10c9f692eccc5
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-10-28 00:11:46 +0300

    Added tips

--- contact.js
+++ contact.js
@@ -1,3 +1,16 @@
+const tips = {"name":"Пример: Иванов Иван Иванович", "email":"Пример: example@example.com",
+    "number":"Пример: +7777777777", "age":"Положительное число от 1 до 150", "text":"Пример: Советы по поводу улучшения сайта"};
+
+const addTip = (input, textContent) => {
+    if ($("#"+input+"-tip").length){
+        return;
+    }
+    $("#"+input+"-container").append("<div class='pop-up-tip' id='"+input+"-tip'> "+textContent+" </div>")
+    setTimeout(() => {
+        $("#"+input+"-tip").remove();
+    }, 3000);
+};
+
 $("#number").on("change", () => {
     setValid($("#number"));
     validateNumber();
@@ -7,6 +20,14 @@ $("#email").on("change", () => {
     setValid($("#email"));
     validateEmail();
 });
+
+$("input").on("click", function() {
+    console.log($(this).attr('id'));
+    if ($(this).attr('id')!=="date"){
+        addTip($(this).attr('id'), tips[$(this).attr('id')]);
+    }
+});
+
 
 $("#but2").on("click", () => {
     setValidElements();
