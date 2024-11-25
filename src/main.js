0d0944bc26df09bf2548c06bfdf24945277f7aa0
coolrunner1 <waurttttt@gmail.com>
2024-11-25 13:06:28 +0300

    Finished lr7

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import {createStore} from "vuex";
-import store from "@/assets/store/index.js";
+import store from "@/store/index.js";
 
 createApp(App).use(router, store).mount('#app')
